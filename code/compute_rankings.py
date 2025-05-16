984d4d9a3a97ba8985a5dcf8f30d0b0728534ce8
Isa Spiero <90386963+isa-sp@users.noreply.github.com>
2025-05-16 09:57:16 +0200

    clean code

--- code/compute_rankings.py
+++ code/compute_rankings.py
@@ -1,38 +1,29 @@
-# Import the packages
-
 import numpy as np
 import pandas as pd
 import pickle
 import os
 import shutil
-import seaborn as sns
-import matplotlib.pyplot as plt
-import argparse
+#import seaborn as sns
+from pathlib import Path
+from asreview import ASReviewData, ASReviewProject
+from asreview.review import ReviewSimulate
+from asreview.models.classifiers import LogisticClassifier, NaiveBayesClassifier #, LSTMBaseClassifier, LSTMPoolClassifier, NN2LayerClassifier, RandomForestClassifier, SVMClassifier
+from asreview.models.query import MaxQuery #, ClusterQuery, MaxRandomQuery, MaxUncertaintyQuery, RandomQuery, UncertaintyQuery
+from asreview.models.balance import DoubleBalance #, SimpleBalance, UndersampleBalance
+from asreview.models.feature_extraction import Doc2Vec, Tfidf #, SBERT, EmbeddingIdf, EmbeddingLSTM,
+from asreview import open_state
 
-# parser = argparse.ArgumentParser(description='Running simulations in  parallel')
-# parser.add_argument('-sim_id', type=int, default=1, help='Number')
-# args = parser.parse_args()
+path_data = '../data/processed/for_ranking/'
+path_results = '../output/tmp/'
+path_results_HPC = '../output/rankings'
 
-path_data = '/Users/ispiero2/TAR-abstracts/data/processed/for_ranking/'  # /home/julius_te/ispiero/systematicreviews/data/data_HPC
-path_results = '/Users/ispiero2/TAR-abstracts/output/output/Results_tmp/'  # /home/julius_te/ispiero/systematicreviews/results
-path_results_HPC = '/Users/ispiero2/TAR-abstracts/output/output/simulations_original_datasets/'
-
-# Import the prognosis datasets (numbering of the datasets is ordered by author, so numbers do not correspond to numbers in data prep)
-
-#os.chdir(path_data)
-
-# Prognosis dataset 1: model reporting
+# Import the systematic review datasets (numbering of the datasets is according to another related research)
 df_prog1 = pd.read_excel(path_data+'Prog_reporting_labeled.xlsx')
-
-# Prognosis dataset 3: tripod
 df_prog3 = pd.read_excel(path_data+'Prog_tripod_labeled.xlsx')
 
-# Create dictionaries of the datasets
-
-review_dic = {'Prog1': df_prog1,
-              'Prog3': df_prog3,
+review_dic = {#'Prog1': df_prog1,
+              'Prog3': df_prog3#,
               }
-
 for review in review_dic:
     review_dic[review]['title'] = review_dic[review]['title'].replace(np.nan, '')
     review_dic[review]['abstract'] = review_dic[review]['abstract'].replace(np.nan, '')
@@ -40,44 +31,16 @@ for review in review_dic:
     review_dic[review]['abstract'] = review_dic[review]['abstract'].astype(str)
 
 # Create a path to store the simulation results
-
 os.chdir(path_results)
-
-from pathlib import Path
-from asreview import ASReviewData, ASReviewProject
-from asreview.review import ReviewSimulate
-
 path_name = "simulation_results_new_evaluation"
-project_path = Path(path_name)  # leave out on HPC
-project_path.mkdir(exist_ok=True)  # leave out on HPC
+project_path = Path(path_name)
+project_path.mkdir(exist_ok=True)
 
-# Define the function for ASReview simulation(s)
-
-# Import the functions from ASReview
-# Classifiers:
-from asreview.models.classifiers import LogisticClassifier, LSTMBaseClassifier, LSTMPoolClassifier, \
-    NaiveBayesClassifier, NN2LayerClassifier, RandomForestClassifier, SVMClassifier
-
-# Query models:
-from asreview.models.query import ClusterQuery, MaxQuery, MaxRandomQuery, MaxUncertaintyQuery, RandomQuery, \
-    UncertaintyQuery
-
-# Balance models:
-from asreview.models.balance import DoubleBalance, SimpleBalance, UndersampleBalance
-
-# Feature extraction models:
-from asreview.models.feature_extraction import Doc2Vec, EmbeddingIdf, EmbeddingLSTM, SBERT, Tfidf
-
-# For evaluation:
-from asreview import open_state
-
-
+# Define the function for ranking simulation(s)
 def ASReview_simulation(review_id, review_data, path_name,
-                        # sim_id, # add on HPC
                         train_model=NaiveBayesClassifier(),
                         query_model=MaxQuery(), balance_model=DoubleBalance(), feature_model=Tfidf(),
-                        n_simulations=10,  # leave out on HPC
-                        n_model_update=10, n_prior_included=10, n_prior_excluded=10):
+                        n_simulations=10, n_model_update=10, n_prior_included=10, n_prior_excluded=10):
     """
         Performs semi-automated title-abstract screening simulations
         based on a labeled review dataset using the open source codes of ASReview.
@@ -106,10 +69,6 @@ def ASReview_simulation(review_id, review_data, path_name,
         n_model_update (int):             number of records of which the screening is simulated before the model is updated
                                                -> Default by ASReview is 10
     """
-
-    # Create a list of numbers ranging from 1 to n_simulations
-    # sim = list(range(1, n_simulations+1)) # sim = sim_id # replace on HPC
-
     # Create an id for the review based on name and models
     review_id = review_id + "_" + str(train_model.name) + "_" + str(feature_model.name) + "_" + str(query_model.name)
 
@@ -120,19 +79,17 @@ def ASReview_simulation(review_id, review_data, path_name,
     dict_rank = {}
 
     # Run the simulations
-    for i in list(range(1, n_simulations + 1)):  # leave out on HPC
+    for i in list(range(118, n_simulations + 1)):
 
         # Set the path for the temporary results
-        # path_name = path_name # leave out on HPC
-        project_path = Path(
-            path_name)  # project_path = Path(path_name+"{y}.{x}_simulation".format(x=sim, y=review_id)) # replace on HPC
+        project_path = Path(path_name)
         project_path.mkdir(exist_ok=True)
 
         # Run the simulation based on the settings and store the output at the project path
-        print("Running simulation number", i)  # print(sim) # replace on HPC
+        print("Running simulation number", i)
         # (Derived from ASReview from here...
         reviewer = ReviewSimulate(
-            init_seed=int(i),  # init_seed=int(sim) # replace on HPC
+            init_seed=int(i),
             as_data=ASReviewData(review_data),
             model=train_model,
             query_model=query_model,
@@ -161,42 +118,26 @@ def ASReview_simulation(review_id, review_data, path_name,
         with open_state(reviewer.project) as s:
             labeled = s.get_labeled()
             priors = s.get_priors()
-            labels = labeled[~labeled['record_id'].isin(priors['record_id'])]
 
             # Derive the rankings of the records and store in the dictionary
-            dict_rank[i] = []  # dict_rank[sim] = [] # replace on HPC
-            dict_rank[i].append(labeled)  # dict_rank[sim].append(labeled) # replace on HPC
+            dict_rank[i] = []
+            dict_rank[i].append(labeled)
 
         # Close and remove the simulation results from the path
         shutil.rmtree(project_path)
 
-        # Save the simulation # add on HPC
-        os.chdir(path_results) # add on HPC
+        # Save the simulation
+        os.chdir("../" + path_results_HPC)
 
-        with open('sim_{review}_{simu}.p'.format(review=review_id, simu=i), 'wb') as f: # add on HPC
-            pickle.dump(dict_rank, f) # add on HPC
+        with open('sim_{review}_{simu}.p'.format(review=review_id, simu=i), 'wb') as f:
+            pickle.dump(dict_rank, f)
 
     # Return the relevant results of the simulations
-    return review_id, review_length, n_simulations, dict_rank  # return review_id, review_length, dict_rank #n_simulations, dict_rank # replace on HPC
+    return review_id, review_length, n_simulations, dict_rank
 
-
-# A test run to test the function
-# test_simulation = ASReview_simulation(review_id='Prog1', review_data=review_dic['Prog1'], path_name=path_name,
-#                                       train_model=NaiveBayesClassifier(), query_model=MaxQuery(),
-#                                       balance_model=DoubleBalance(), feature_model=Tfidf(),
-#                                       n_simulations=10,
-#                                       n_model_update=10, n_prior_included=10, n_prior_excluded=10)
-
-# To assess the performance of ASReview, the reviews, the classification models,
-# feature extraction models, and/or query models are varied.
-
-# Define the classification model(s) to be tested
+# Define the model(s) to be tested
 train_models = [LogisticClassifier()]
-
-# Define the feature extraction model(s) to be tested
-feature_models = [EmbeddingLSTM(), Doc2Vec()]  # EmbeddingIdf() does not work
-
-# Define the query model(s) to be tested (for now no variation in query models)
+feature_models = [Doc2Vec()]
 query_models = [MaxQuery()]
 
 # Test each different method (and interactions) with the following loop for Tfidf():
@@ -207,10 +148,10 @@ for review in review_dic:
         for feature_model in feature_models:
             for query_model in query_models:
                 sim_list.append([review, train_model, feature_model, query_model])
-                sim = ASReview_simulation(review_id = review, review_data = review_dic[review], # leave out on HPC
+                sim = ASReview_simulation(review_id = review, review_data = review_dic[review],
                                           path_name = path_name,
                                           train_model = train_model, query_model = query_model,
                                           balance_model = DoubleBalance(), feature_model = feature_model,
                                           n_simulations = 200, n_model_update = 10,
                                           n_prior_included = 10, n_prior_excluded = 10)
-                multiple_sims_saved.append(sim) # leave out on HPC
+                multiple_sims_saved.append(sim)
